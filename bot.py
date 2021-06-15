864211187cd3d6d9be375ae70ae2bc2518eeb5f1
Samith Shetty <sshett15@uncc.edu>
2021-06-14 23:56:56 -0400

    Fixed bugs, everything should work now

--- bot.py
+++ bot.py
@@ -3,9 +3,9 @@ from discord.ext import commands
 import DiscordUtils
 import pandas as pd
 from typing import Optional
+import os
 
 
-token = 'ODUzODQ4NTg2MzQ0MDcxMjE4.YMbWdg.MG0LrkUPtChuwxAJxNWfltiF_m4'
 intents = discord.Intents(messages = True, guilds = True, reactions = True, members = True, presences = False)
 bot = commands.Bot(command_prefix = "-", intents = intents)
 tracker = DiscordUtils.InviteTracker(bot)
@@ -15,16 +15,13 @@ inviters_df = pd.read_csv('csv/member_invite_record.csv', index_col = 'Member')
 leavers_df = pd.read_csv('csv/member_leaver_record.csv', index_col = 'Leaver')
 role_df = pd.read_csv('csv/roles.csv', index_col='Role_ID')
 
-server = channel = None
-
 
 @bot.event
 async def on_ready():
     global server, channel
     await tracker.cache_invites()
-    server = bot.get_guild(701889662267949076)
-    channel = server.get_channel(772543542118711346)
     print("Bot is online")
+
 
 #Command to display invate count values
 @bot.command(aliases=['invites', 'invite', 'count'])
@@ -69,11 +66,13 @@ async def set_invites(ctx, member: discord.Member, real:int, left:int, fake:int)
 # Helper functions for roles, add/remove invite-roles based on current invite count
 async def give_roles(member):
     unlocked_roles = role_df[role_df["Invite_Count"] <= count_df.loc[member.id,"Real"]]
+    if not isinstance(unlocked_roles,pd.DataFrame): unlocked_roles = pd.DataFrame([unlocked_roles]) #cast to DF if it isnt already
     for role_entry in unlocked_roles.itertuples():
         await member.add_roles(member.guild.get_role(role_entry.Index))
 
 async def remove_roles(member):
     unlocked_roles = role_df[role_df["Invite_Count"] > count_df.loc[member.id,"Real"]]
+    if not isinstance(unlocked_roles,pd.DataFrame): unlocked_roles = pd.DataFrame([unlocked_roles]) #cast to DF if it isnt already
     for role_entry in unlocked_roles.itertuples():
         await member.remove_roles(member.guild.get_role(role_entry.Index))
 
@@ -104,7 +103,6 @@ async def on_guild_role_delete(role):
     await role.guild.system_channel.send(f'{role.name} has been deleted and will no longer be automatically assigned')
 
 
-
 #When a member joins, check who invited them and update that member's invite count/record 
 @bot.event
 async def on_member_join(member):
@@ -123,19 +121,24 @@ async def on_member_join(member):
         count_df = count_df.append(new_entry)
     else: #If yes, then add to it
         count_df.loc[inviter.id,"Real"] += 1
-    count_df.to_csv('csv/invite_count.csv', index_label='User_ID')
+    
 
     #Update inviter/leaver record
     inviters_df = inviters_df.append(pd.DataFrame([[inviter.id]], index=[member.id], columns=inviters_df.columns))
     inviters_df.to_csv('csv/member_invite_record.csv', index_label='Member')
 
     try:
-        leavers_df.loc[member.id]
+        entries =leavers_df.loc[member.id]
     except KeyError:
         return #Member has not left the guild before, no need to update anything
     else:
+        for entry in pd.DataFrame([entries]).itertuples():
+            if entry.Inviter == inviter.id:
+                count_df.loc[inviter.id,"Left"] -= 1
+        count_df.to_csv('csv/invite_count.csv', index_label='User_ID')
         leavers_df.drop(labels=member.id, inplace=True)
         leavers_df.to_csv('csv/member_leaver_record.csv', index_label='Leaver')
+    
     await give_roles(inviter)
     await member.guild.system_channel.send(f'{inviter.mention} invited {member.mention} to the server!')
 
@@ -164,24 +167,6 @@ async def on_member_remove(member):
 
         await remove_roles(member.guild.get_member(inviter)) #Update roles of inviter 
 
-
-
-
-@bot.command()
-async def execute(ctx):
-    await ctx.send("Entering execute mode, the bot will now be unable to respond to commands. Insert statements to be executed in terminal, type \"break\" in terminal to exit.")
-    while(True):
-        inp = input("> ")
-        if inp.lower() != 'break':
-            try:
-                exec(inp)
-            except Exception as e:
-                print(e)
-        else:
-            break
-    print("Execution mode has been halted from terminal. Now resuming normal bot functions...")
-    await ctx.send("Execution mode has been halted from terminal. Now resuming normal bot functions...")
-
 ##Invite Tracker Setup
 @bot.event
 async def on_invite_create(invite):
@@ -200,4 +185,4 @@ async def on_guild_remove(guild):
     await tracker.remove_guild_cache(guild)
 
 
-bot.run(token)
+bot.run(os.environ['DISCORD_TOKEN'])
